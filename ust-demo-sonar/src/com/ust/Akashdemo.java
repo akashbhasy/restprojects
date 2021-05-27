8780a3b1b0d119eed7f1e9f9a595ad5b53e7cd86
ustjava16 <ustjava16@iiht.tech>
2021-05-27 13:27:29 +0530

    msg

---
+++ ust-demo-sonar/src/com/ust/Akashdemo.java
@@ -0,0 +1,12 @@
+package com.ust;
+
+public class Akashdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sonar program");
+
+	}
+
+}
